5d8017dee887fefb6d13000cfb00bd3d492d7067
Ganeshrockz <ganeshrockz@github.com>
2020-12-08 00:11:09 +0530

    Minor changes

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hi this is a sample action")
+import os
+
+def main():
+    my_input = os.environ["INPUT_NAME"]
+
+    my_output = f"Hello {my_input}"
+
+    print(f"::set-output name=output::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
