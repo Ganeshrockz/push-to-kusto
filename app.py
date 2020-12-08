f2a4d90bc29e1b0af7a5b4b09d0100697e74e48d
Ganeshrockz <ganeshrockz@github.com>
2020-12-08 14:19:50 +0530

    Added kusto client

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
 import json
 from azure.storage.blob import BlockBlobService, PublicAccess
+from azure.kusto.data import KustoClient, KustoConnectionStringBuilder
+from azure.kusto.data.exceptions import KustoServiceError
+from azure.kusto.data.helpers import dataframe_from_result_table
+from azure.kusto.ingest import KustoIngestClient, IngestionProperties, FileDescriptor, BlobDescriptor, DataFormat, ReportLevel, ReportMethod
 
 def main():
-    # Create blob client
-
+    # Blob inputs
     storageAccountName = os.environ["INPUT_STORAGEACCOUNT"]
     storageAccountKey = os.environ["INPUT_STORAGEACCOUNTKEY"]
     containerName = os.environ["INPUT_CONTAINER"]
+    
+    # Kusto cluster inputs
+    tenantId = os.environ["INPUT_TENANTID"]
+    databaseName = os.environ["INPUT_DATABASE"]
+    clusterName = os.environ["INPUT_CLUSTERNAME"]
+    region = os.environ["INPUT_CLUSTERREGION"]
+    clientId = os.environ["INPUT_CLIENTID"]
+    clientSecret = os.environ["INPUT_CLIENTSECRET"]
+    destinationTable = os.environ["INPUT_TABLE"]
 
     try:
+        # Create blob client
         blob_service_client = BlockBlobService(
             account_name=storageAccountName, account_key=storageAccountKey)
 
@@ -29,6 +42,27 @@ def main():
         print("Uploaded to blob storage")
         
         os.remove(filePath)
+
+        # Blob creation finished
+
+        # Push blob to kusto
+
+        httpsPrefix = "https://"
+        suffixKustoUri = ".kusto.windows.net:443/"
+        clusterUri = "{0}{1}.{2}.{3}".format(httpsPrefix, clusterName, region, suffixKustoUri)
+        clusterIngestUri = "{0}ingest-{1}.{2}.{3}".format(httpsPrefix, clusterName, region, suffixKustoUri)
+
+        kcsb_ingest = KustoConnectionStringBuilder.with_aad_application_key_authentication(
+                        clusterIngestUri, clientId, clientSecret, tenantId)
+
+        blobUri = "https://{0}.blob.core.windows.net/{1}/{2}".format(storageAccountName, containerName, fileName)
+
+        ingestionClient = KustoIngestClient(kcsb_ingest)
+        ingestionProperties = IngestionProperties(database=databaseName, table=destinationTable, dataFormat=DataFormat.JSON)
+        blobDescriptor = BlobDescriptor(blobUri)
+        ingestionClient.ingest_from_blob(blobDescriptor, ingestion_properties=ingestionProperties)
+
+        print('Done queuing up ingestion with Azure Data Explorer')
     except Exception as e:
         print(e)
 
