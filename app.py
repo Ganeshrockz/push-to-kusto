81edc1d9a1d071406653efe4808eca721865e3f9
Ganeshrockz <ganeshrockz@github.com>
2020-12-08 13:08:45 +0530

    Added more inputs

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ import os
 from azure.storage.blob import BlockBlobService, PublicAccess
 
 def main():
-    my_input = os.environ["INPUT_NAME"]
+    # Create blob client
 
-    my_output = f"Hello {my_input}"
+    storageAccountName = os.environ["INPUT_STORAGEACCOUNT"]
+    storageAccountKey = os.environ["INPUT_STORAGEACCOUNTKEY"]
+    containerName = os.environ["INPUT_CONTAINER"]
 
-    file_name = "sample.txt"
-    file_path = os.path.join(os.environ["GITHUB_WORKSPACE"], file_name)
+    try:
+        blob_service_client = BlockBlobService(
+            account_name=storageAccountName, account_key=storageAccountKey)
 
-    f = open(file_path, "w")
-    f.write("sample")
-    f.close()
+        fileName = "test.txt"
+        filePath = os.path.join(os.environ["GITHUB_WORKSPACE"], file_name)
 
-    f = open(file_path, "r")
-    print(f.read())
-    print(my_output)
+        f = open(filePath, "w")
+        f.write("test string")
+        f.close()
+
+        blob_service_client.create_blob_from_path(
+            containerName, fileName, filePath)
+        
+        print("Uploaded to blob storage")
+        
+        os.remove(filePath)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
