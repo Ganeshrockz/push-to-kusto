bcd9f62c895ce104868f6011b1b3025f66f480b4
Ganeshrockz <ganeshrockz@github.com>
2020-12-08 12:05:37 +0530

    create temp file

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ def main():
 
     my_output = f"Hello {my_input}"
 
-    print(os.environ["RUNNER_TEMP"])
+    file_name = "sample.txt"
+    file_path = os.path.join(os.environ["RUNNER_TEMP"], file_name)
+
+    f = open(file_path, "w")
+    f.write("sample")
+    f.close()
+
+    f = open(file_path, "r")
+    print(f.read())
     print(my_output)
 
 
